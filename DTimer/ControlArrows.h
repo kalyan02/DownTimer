e1fd6ae8e57f332e6a3ede13119958d7b11d0b36
Kalyan <nvsnkalyan@gmail.com>
2013-12-15 12:55:04 +0100

    Control arrows for the mins and seconds fields

---
+++ DTimer/ControlArrows.h
@@ -0,0 +1,13 @@
+//
+//  ControlArrows.h
+//  DownTimer
+//
+//  Created by Kalyan on 15/12/13.
+//  Copyright (c) 2013 KC. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface ControlArrows : NSView
+
+@end
---
+++ DTimer/ControlArrows.m
@@ -0,0 +1,29 @@
+//
+//  ControlArrows.m
+//  DownTimer
+//
+//  Created by Kalyan on 15/12/13.
+//  Copyright (c) 2013 KC. All rights reserved.
+//
+
+#import "ControlArrows.h"
+
+@implementation ControlArrows
+
+- (id)initWithFrame:(NSRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self) {
+        // Initialization code here.
+    }
+    return self;
+}
+
+- (void)drawRect:(NSRect)dirtyRect
+{
+	[super drawRect:dirtyRect];
+	
+    // Drawing code here.
+}
+
+@end
